49dc192537b058c01fb2a60d6122e41f1164148e
Andrew <drew.l.connell@gmail.com>
2019-09-10 14:59:01 -0400

    Initial Project

---
+++ Assets/_360VideoPlayer/Scripts/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void OculusInput()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.All))
+        {
+
+        }
+
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+        {
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+        {
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+
+        }
+    }
+
+    private void KeyboardInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+    }
+}
---
+++ Assets/_360VideoPlayer/Scripts/VideoManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    
+}
